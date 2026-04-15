7e6ce40dfdcf6710c98fa43a0ecc9353fe9538bd
Luiz Paulo Baçal de Vasconcelos <luizv993@gmail.com>
2026-04-15 21:53:36 +0000

    feat: add new files database.js and .env

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # clone-tabnews
 
 Implementação do tabnews para o curso.dev
+
+# comandos utilizados
+
+docker compose -f infra/compose.yaml up (start do banco postgres, utilize -d no final para rodar no modo detach)
---
+++ infra/database.js
@@ -0,0 +1,17 @@
+import { Cliente } from "pg";
+
+async function query(queryObject) {
+  const cliente = new Cliente({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  await cliente.connect();
+  const result = await cliente.query(queryObject);
+  await cliente.end();
+  return result;
+}
+
+export default {};
