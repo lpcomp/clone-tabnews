7ed6cdd3eed67b8ff346b476e08dc6f90d6e117a
Luiz Paulo Baçal de Vasconcelos <luizv993@gmail.com>
2026-04-17 00:51:33 +0000

    fix: fix databse connect

--- infra/database.js
+++ infra/database.js
@@ -1,17 +1,21 @@
-import { Cliente } from "pg";
+import { Client } from "pg";
 
 async function query(queryObject) {
-  const cliente = new Cliente({
+  const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await cliente.connect();
-  const result = await cliente.query(queryObject);
-  await cliente.end();
+  await client.connect();
+  const result = await client.query(queryObject);
+  await client.end();
   return result;
 }
 
-export default {};
+const database = {
+  query,
+};
+
+export default database;
